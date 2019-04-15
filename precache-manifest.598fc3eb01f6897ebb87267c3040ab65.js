4c6de33aff69b468af6caf80203982c53c5450a8
Erick Wendel <erick.workspace@gmail.com>
2019-04-14 23:26:49 -0700

    Updates

---
+++ precache-manifest.598fc3eb01f6897ebb87267c3040ab65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c9604bb02127ab56e3c",
+    "url": "/static/js/main.162d2568.chunk.js"
+  },
+  {
+    "revision": "41e96dffe018d6787ee8",
+    "url": "/static/js/2.8a4156cd.chunk.js"
+  },
+  {
+    "revision": "8c9604bb02127ab56e3c",
+    "url": "/static/css/main.b2c8e565.chunk.css"
+  },
+  {
+    "revision": "501d83adbe32f58f3502b5b6668755d5",
+    "url": "/index.html"
+  }
+];
