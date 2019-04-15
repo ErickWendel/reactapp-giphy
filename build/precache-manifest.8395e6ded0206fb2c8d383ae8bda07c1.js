9543d3973caf4d9534cedbe0b3ad163a1f64bb32
Erick Wendel <erick.workspace@gmail.com>
2019-04-14 23:31:00 -0700

    added gh pages files
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ build/precache-manifest.8395e6ded0206fb2c8d383ae8bda07c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97ff515be11fb8ce65cf",
+    "url": "/reactapp-giphy/static/js/runtime~main.eb67b1b4.js"
+  },
+  {
+    "revision": "6994b087deea94bfba32",
+    "url": "/reactapp-giphy/static/js/main.be497ff2.chunk.js"
+  },
+  {
+    "revision": "41e96dffe018d6787ee8",
+    "url": "/reactapp-giphy/static/js/2.8a4156cd.chunk.js"
+  },
+  {
+    "revision": "6994b087deea94bfba32",
+    "url": "/reactapp-giphy/static/css/main.b2c8e565.chunk.css"
+  },
+  {
+    "revision": "0ff72d13a9350aa249220d41c79564b3",
+    "url": "/reactapp-giphy/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactapp-giphy/precache-manifest.8395e6ded0206fb2c8d383ae8bda07c1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactapp-giphy/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
